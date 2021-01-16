2a66237c9f2c2b31c12e1ca5abb7f688a3975c2c
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2021-01-16 23:31:14 +0300

    call-modal & adaptive menu stylized

--- js/script.js
+++ js/script.js
@@ -14,3 +14,27 @@ jQuery(function() {
     ]
   });
 });
+
+
+let takeButton = document.querySelector('.header__take-btn');
+let callModal = document.querySelector('.call-modal');
+let callClose = document.querySelector('.call-modal__close');
+
+takeButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  callModal.classList.add('active');
+});
+
+callClose.addEventListener('click', function() {
+  callModal.classList.remove('active');
+});
+
+let menuBtn = document.querySelector('.header__menu');
+let menuLine = document.querySelectorAll('.header__menu-line');
+let mobileNav = document.querySelector('.mobile-nav');
+
+menuBtn.addEventListener('click', function() {
+  this.classList.toggle('active');
+  menuLine.forEach((item) => item.classList.toggle('active'));
+  mobileNav.classList.toggle('active');
+});
